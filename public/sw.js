51d3650b73a7236c431eafdcffccc6da53ff59bc
Kentaro Kuribayashi <kentarok@gmail.com>
2024-07-10 22:57:58 +0900

    やっぱり同じエラーがでますよ。
    TypeError: Failed to execute 'addAll' on 'Cache': Request failed'

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,25 @@
+const CACHE_NAME = 'todo-app-v1';
+const urlsToCache = [
+  '/todo/',
+  '/todo/index.html',
+  '/todo/manifest.json',
+  '/todo/icon-192x192.png',
+  '/todo/icon-512x512.png'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('todo-app-v1').then((cache) => {
-      return cache.addAll([
-        '/todo/',
-        '/todo/index.html',
-        '/todo/_next/static/chunks/main.js',
-        '/todo/_next/static/chunks/pages/_app.js',
-        '/todo/_next/static/chunks/pages/index.js',
-        '/todo/_next/static/css/main.css',
-        '/todo/manifest.json',
-        '/todo/icon-192x192.png',
-        '/todo/icon-512x512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .catch((error) => console.error('Cache addAll error:', error))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+      .catch((error) => console.error('Fetch error:', error))
   );
 });
 
@@ -28,10 +27,10 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.filter((cacheName) => {
-          return cacheName.startsWith('todo-app-') && cacheName !== 'todo-app-v1';
-        }).map((cacheName) => {
-          return caches.delete(cacheName);
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
         })
       );
     })
