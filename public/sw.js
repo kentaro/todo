7b58cd09decf26f46268be0127ce921d6ae07c42
Kentaro Kuribayashi <kentarok@gmail.com>
2024-07-10 22:54:24 +0900

    1. AndrodのChromeだとアプリとしてインストールできないようです。 2. TypeError: Failed to execute 'addAll' on 'Cache': Request failed

--- public/sw.js
+++ public/sw.js
@@ -2,10 +2,15 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('todo-app-v1').then((cache) => {
       return cache.addAll([
-        '/todo',
+        '/todo/',
         '/todo/index.html',
         '/todo/_next/static/chunks/main.js',
-        // その他の必要なアセットを追加
+        '/todo/_next/static/chunks/pages/_app.js',
+        '/todo/_next/static/chunks/pages/index.js',
+        '/todo/_next/static/css/main.css',
+        '/todo/manifest.json',
+        '/todo/icon-192x192.png',
+        '/todo/icon-512x512.png'
       ]);
     })
   );
@@ -15,6 +20,20 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName.startsWith('todo-app-') && cacheName !== 'todo-app-v1';
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
