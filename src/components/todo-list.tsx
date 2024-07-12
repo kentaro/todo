f6abb4db2d079db8792a7d441cea2c39369b581f
Kentaro Kuribayashi <kentarok@gmail.com>
2024-07-12 11:21:03 +0900

    todo itemについて、チェックしたら下の方にさげてほしいです。未完了のタスクは、期限が近いものを一番上に表示してください。

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,5 +1,6 @@
 import { Todo } from '@/types'
 import { TodoItem } from './todo-item'
+import { useEffect, useState } from 'react'
 
 type TodoListProps = {
   todos: Todo[]
@@ -9,15 +10,40 @@ type TodoListProps = {
 }
 
 export function TodoList({ todos, onToggle, onDelete, onUpdate }: TodoListProps) {
+  const [animatedTodoId, setAnimatedTodoId] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (todos.length > 0) {
+      const latestTodo = todos[0];
+      setAnimatedTodoId(latestTodo.id);
+      const timer = setTimeout(() => setAnimatedTodoId(null), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [todos]);
+
+  const sortedTodos = [...todos].sort((a, b) => {
+    if (a.completed !== b.completed) {
+      return a.completed ? 1 : -1;
+    }
+    if (!a.completed && !b.completed) {
+      if (a.dueDate && b.dueDate) {
+        return new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime();
+      }
+      return a.dueDate ? -1 : 1;
+    }
+    return b.id - a.id;
+  });
+
   return (
     <div>
-      {todos.map((todo) => (
+      {sortedTodos.map((todo) => (
         <TodoItem
           key={todo.id}
           todo={todo}
           onToggle={onToggle}
           onDelete={onDelete}
           onUpdate={onUpdate}
+          isAnimated={todo.id === animatedTodoId}
         />
       ))}
     </div>
