cbdc14b60c7a9a4b48de1cb7dad6c5bc5ad44da4
Kentaro Kuribayashi <kentarok@gmail.com>
2024-07-12 11:25:02 +0900

    やっぱりサービスワーカーのキャッシュのせいなのかデプロイしたあとにブラウザ上での表示がおかしくなります。スタイルがあたらなくなる。そもそもキャッシュしないとならないんですか？

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,8 @@
 const VERSION = '__GIT_REF__';
 const CACHE_NAME = `todo-app-${VERSION}`;
 
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
 
 self.addEventListener('activate', (event) => {
